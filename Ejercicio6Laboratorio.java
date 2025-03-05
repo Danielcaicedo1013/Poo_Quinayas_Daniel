f19ce98ed42dcdf98a1bf825c504be2d456691ab
Danielcaicedo1013 <u20242227875@usco.edu.co>
2025-03-05 08:58:41 -0500

    Ejercicio6Laboratorio.java

--- Ejercicio6Laboratorio.java
+++ Ejercicio6Laboratorio.java
@@ -2,7 +2,7 @@
 determina si una persona tiene anemia o no, lo cual depende de su nivel de hemoglobina en la
 sangre, de su edad y de su sexo. Si el nivel de hemoglobina que tiene una persona es menor
 que el rango que le corresponde, se determina su resultado como positivo y en caso contrario
-como negativo. La tabla en la que el medico se basa para obtener el resultado es la siguiente:
+como negativo. La tabla en la que el medico se basa para obtener el resultado es la siguiente.:
 
 EDAD                        NIVEL HEMOGLOBINA
 0 - 1 mes                   13 - 26 g%
